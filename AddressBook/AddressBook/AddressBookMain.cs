3951c6984b18d241293c9572cd843a20e75c4c03
Manish <manishkumar0156@gmail.com>
2022-02-03 15:07:43 +0530

    [Manish] added new contact in Address book

---
+++ AddressBook/AddressBook/AddressBookMain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddressBookMain
+    {
+        List<Contacts> addressBook=new List<Contacts>();
+        public void CreateContact()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Enter FirstName: \n");
+            contacts.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Lastline: \n");
+            contacts.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter PhoneNumber: \n");
+            contacts.PhoneNumber = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Email ID: \n");
+            contacts.EmailId = Console.ReadLine();
+
+            Console.WriteLine("Enter Address: \n");
+            contacts.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter City: \n");
+            contacts.City = Console.ReadLine();
+
+            Console.WriteLine("Enter State: \n");
+            contacts.State = Console.ReadLine();
+
+            Console.WriteLine("Enter ZipCode: \n");
+            contacts.ZipCode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Create Contact:- \n" + contacts.FirstName + "\n" + contacts.LastName +
+                "\n" + contacts.PhoneNumber + "\n" + contacts.EmailId + "\n" + contacts.Address + "\n"
+                + contacts.City + "\n" + contacts.State + "\n" + contacts.ZipCode);
+           
+            addressBook.Add(contacts);
+        }
+    }
+}
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -13,34 +13,7 @@ namespace AddressBook
             Console.WriteLine("Welcome to Address Book Problem Statement!");
             Contacts contacts = new Contacts();
 
-            Console.WriteLine("Enter FirstName: \n");
-            contacts.FirstName=Console.ReadLine();
-
-            Console.WriteLine("Enter Lastline: \n");
-            contacts.LastName=Console.ReadLine();
-
-            Console.WriteLine("Enter PhoneNumber: \n");
-            contacts.PhoneNumber = long.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter Email ID: \n");
-            contacts.EmailId=Console.ReadLine();    
-
-            Console.WriteLine("Enter Address: \n");
-            contacts.Address=Console.ReadLine();
-
-            Console.WriteLine("Enter City: \n");
-            contacts.City=Console.ReadLine();
-
-            Console.WriteLine("Enter State: \n");
-            contacts.State = Console.ReadLine();
-
-            Console.WriteLine("Enter ZipCode: \n");
-            contacts.ZipCode = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Create Contact:- \n"+contacts.FirstName+"\n"+contacts.LastName+
-                "\n"+contacts.PhoneNumber+"\n"+contacts.EmailId+"\n"+contacts.Address+"\n"
-                +contacts.City+"\n"+contacts.State+"\n"+contacts.ZipCode);
-            
+         
         }
     }
 }
